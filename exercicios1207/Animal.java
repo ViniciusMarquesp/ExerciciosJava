c7b5bdd05280733b6c4d7e1535dc4a9be8fadb97
ViniciusMarquesp <ciusivinicius@hotmail.com>
2021-07-12 14:01:34 -0300

    Exercicio Interface

---
+++ exercicios1207/Animal.java
@@ -0,0 +1,10 @@
+package br.com.generation.exercicios1207;
+
+public interface Animal {
+
+	public void nome();
+	public void idade();
+	public void pelo();
+	public void emiteSom();
+	public void cor();
+}
